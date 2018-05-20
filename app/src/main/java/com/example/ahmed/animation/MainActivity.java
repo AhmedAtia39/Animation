3bc80e51e8904db6174d8e89559b0f0527c5c559
AhmedAtia39 <ahmedatia689@gmail.com>
2018-05-20 18:08:29 +0200

    done

--- app/src/main/java/com/example/ahmed/animation/MainActivity.java
+++ app/src/main/java/com/example/ahmed/animation/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.ahmed.animation;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button1);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim);
+        btn.setAnimation(animation);
     }
 }
